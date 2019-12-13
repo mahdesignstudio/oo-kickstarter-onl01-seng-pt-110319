348c5bedb6eb20470a34905adbfe4e194595cd75
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-13 02:08:56 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -7,7 +7,7 @@ class Backer
     @backed_projects = [ ]
     end 
     
-  def backed_projects(project)
+  def backed_project(project)
     @backed_projects << project
     project.backers << self 
   end 
